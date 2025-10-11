e4217ba95651ded63610de5b629f548c053787e8
Koustav Manna <koustaviitjee@gmail.com>
2025-10-11 22:59:29 +0530

    some minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,13 +78,13 @@ export default function Home() {
         <img src="image/56.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[30vw] -translate-y-[16vh]" />
         <img src="image/67.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[23vw] -translate-y-[35vh]" />
         <img src="image/78.jpg" className="h-[8rem] rounded-xl absolute translate-x-[27vw] -translate-y-[25vh]" />
-        <div className="Body bg-pink-300 h-[35rem] w-[35rem] rounded-[20px] flex justify-center items-center">
-            <div className="setMainImageSrc"><img src={mainImageSrc} alt="image" className="h-[25rem] w-[28rem] rounded-[20px]" />
+        <div className="Body bg-pink-300 h-[35rem] w-[35rem] min-h-[15rem] min-w-[15rem] rounded-[20px] flex justify-center items-center">
+            <div className="setMainImageSrc"><img src={mainImageSrc} alt="image" className="h-[25rem] w-[28rem] min-h-[5rem] min-w-[8rem] rounded-[20px]" />
                 <h3 className=" text-3xl font-semibold text-cyan-800">{innerText}</h3>
                 <div className=" flex justify-evenly items-center h-[2.8rem]">
                   <div className="rotating-gradient Body bg-gradient-to-r from-purple-500 via-sky-400 to-emerald-400 h-[48px] w-[208px] flex justify-center items-center rounded-[5px]  hover:cursor-pointer hover:duration-100 hover:scale-110 active:scale-200"><button className="Body bg-rose-500 rounded-[5px] h-[2.5rem] w-[12.5rem] text-3xl hover:cursor-pointer hover:duration-100 transition-all " onClick={handleYesClick}>YES</button></div>
                     
-                    <button className="Body bg-rose-500 rounded-[5px] h-[2.5rem] w-[12.5rem] text-[10px] hover:cursor-pointer hover:scale-90 transition-all hover:duration-75 active:scale-75" onMouseEnter={handleNoChange} onClick={handleNoClick}>NO</button>
+                    <button className="Body bg-rose-500 rounded-[5px] h-[2.5rem] w-[12.5rem] text-[10px] hover:cursor-pointer hover:scale-90 transition-all hover:duration-75 active:scale-75 z-2" onMouseEnter={handleNoChange} onClick={handleNoClick}>NO</button>
                 </div>
             </div>
           </div>
