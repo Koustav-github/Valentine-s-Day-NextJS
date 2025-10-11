94eda24bce06a6e1b91947c5a652f7f62883066d
Koustav Manna <koustaviitjee@gmail.com>
2025-10-11 18:06:35 +0530

    changed units to rem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,23 +55,23 @@ export default function Home() {
   }
 
   return (
-    <div style={{height:"100vh", width:"100vw", alignItems:"center"}} className="Body, bg-amber-200 flex justify-center ">
+    <div style={{height:"100vh", width:"100vw", alignItems:"center"}} className="Body, bg-amber-200 flex justify-center">
       <audio ref={audioRef} src="audios/meow2.mp3" ></audio>
       <audio ref={audioRef2} src="audios/done.mp3" loop></audio>
-        <img src="image/12.jpg" className="h-[125px] rounded-xl absolute translate-x-[500px] translate-y-[200px]" />
-        <img src="image/23.jpg" className="h-[125px] rounded-xl absolute translate-x-[400px]" />
-        <img src="image/34.jpg" className="h-[125px] rounded-xl absolute -translate-x-[500px]" />
-        <img src="image/45.jpg" className="h-[125px] rounded-xl absolute -translate-x-[600px] translate-y-[170px]" />
-        <img src="image/56.jpg" className="h-[125px] rounded-xl absolute -translate-x-[600px] -translate-y-[170px]" />
-        <img src="image/67.jpg" className="h-[125px] rounded-xl absolute -translate-x-[450px] -translate-y-[250px]" />
-        <img src="image/78.jpg" className="h-[125px] rounded-xl absolute translate-x-[500px] -translate-y-[200px]" />
-        <div className="Body bg-pink-300 h-[80vh] w-[40vw] rounded-[20px] flex justify-center items-center">
-            <div className="setMainImageSrc"><img src={mainImageSrc} alt="image" className="h-[60vh] w-[30vw] rounded-[20px]" />
-                <h3 className="text-black text-3xl font-semibold mask-linear-from-neutral-100">{innerText}</h3>
-                <div className="flex justify-evenly items-center h-[40px]">
-                  <div className="rotating-gradient Body bg-gradient-to-r from-purple-500 via-sky-400 to-emerald-400 h-[48px] w-[208px] flex justify-center items-center rounded-[5px]  hover:cursor-pointer hover:duration-100 hover:scale-110 active:scale-200"><button className="Body bg-rose-500 rounded-[5px] h-[40px] w-[200px] text-3xl hover:cursor-pointer hover:duration-100 transition-all " onClick={handleYesClick}>YES</button></div>
+        <img src="image/12.jpg" className="h-[8rem] rounded-xl absolute translate-x-[30vw] translate-y-[28vh]" />
+        <img src="image/23.jpg" className="h-[8rem] rounded-xl absolute translate-x-[25vw] translate-y-[3vh]" />
+        <img src="image/34.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[25vw] translate-y-[3vh]" />
+        <img src="image/45.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[30vw] translate-y-[25vh]" />
+        <img src="image/56.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[30vw] -translate-y-[16vh]" />
+        <img src="image/67.jpg" className="h-[8rem] rounded-xl absolute -translate-x-[23vw] -translate-y-[35vh]" />
+        <img src="image/78.jpg" className="h-[8rem] rounded-xl absolute translate-x-[27vw] -translate-y-[25vh]" />
+        <div className="Body bg-pink-300 h-[35rem] w-[35rem] rounded-[20px] flex justify-center items-center">
+            <div className="setMainImageSrc"><img src={mainImageSrc} alt="image" className="h-[25rem] w-[28rem] rounded-[20px]" />
+                <h3 className=" text-3xl font-semibold text-cyan-800">{innerText}</h3>
+                <div className=" flex justify-evenly items-center h-[2.8rem]">
+                  <div className="rotating-gradient Body bg-gradient-to-r from-purple-500 via-sky-400 to-emerald-400 h-[48px] w-[208px] flex justify-center items-center rounded-[5px]  hover:cursor-pointer hover:duration-100 hover:scale-110 active:scale-200"><button className="Body bg-rose-500 rounded-[5px] h-[2.5rem] w-[12.5rem] text-3xl hover:cursor-pointer hover:duration-100 transition-all " onClick={handleYesClick}>YES</button></div>
                     
-                    <button className="Body bg-rose-500 rounded-[5px] h-[40px] w-[200px] text-[10px] hover:cursor-pointer hover:scale-90 transition-all hover:duration-75 active:scale-75" onMouseEnter={handleNoChange} onClick={handleNoClick}>NO</button>
+                    <button className="Body bg-rose-500 rounded-[5px] h-[2.5rem] w-[12.5rem] text-[10px] hover:cursor-pointer hover:scale-90 transition-all hover:duration-75 active:scale-75" onMouseEnter={handleNoChange} onClick={handleNoClick}>NO</button>
                 </div>
             </div>
           </div>
